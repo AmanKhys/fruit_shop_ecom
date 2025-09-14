a680133a870f77514722d4fe56b48fc1f3f4a4ed
AmanKhys <amankhys2112@gmail.com>
2025-09-15 03:21:49 +0530

    change user_service ServerStartURL and dbdsn key

--- user_service/internal/config/env.go
+++ user_service/internal/config/env.go
@@ -2,8 +2,8 @@ package config
 
 const (
 	JWTSecret      = "JWT_SECRET"
-	DBDSN          = "DB_DSN"
-	ServerStartURL = "SERVER_START_URL"
+	DBDSN          = "USER_DB_DSN"
+	ServerStartURL = "USER_SERVICE_URL"
 	AdminEmail     = "ADMIN_EMAIL"
 	AdminPassword  = "ADMIN_PASSWORD"
 )

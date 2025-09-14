da05793f799f294280ace5037641b30c55af45ef
AmanKhys <amankhys2112@gmail.com>
2025-09-14 12:25:18 +0530

    move handlers.go to internal/delivery/http/handlers.go; making the structure precise with clean architecture;

--- user_service/internal/delivery/http/server.go
+++ user_service/internal/delivery/http/server.go
@@ -2,10 +2,9 @@ package http
 
 import (
 	"net/http"
-	"user_service/handlers"
 )
 
-func RegisterRoutes(h *handlers.UserHandler) {
+func RegisterRoutes(h *UserHandler) {
 	http.HandleFunc("POST /register", h.Register)
 	http.HandleFunc("POST /login", h.Login)
 }
